6202b9f33ec13640e8739eb800b3126f02b6a3b4
Yash <yasharmag01@gmail.com>
2024-08-21 01:42:54 +0530

    Update AccountNavigation.jsx

--- bookingwebapp/src/pages/AccountNavigation.jsx
+++ bookingwebapp/src/pages/AccountNavigation.jsx
@@ -1,12 +1,36 @@
+import { useEffect, useState } from "react";
 import { Link, useLocation } from "react-router-dom";
+import axios from "axios";
 
 export default function AccountNavigation(){
+
+    const [userRole,setUserRole] = useState('');
 
     const {pathname} =  useLocation();
     let subpage = pathname.split('/')?.[2];
     if(subpage === undefined){
         subpage = 'profile';
     }
+
+    useEffect(() => {
+        const fetchUserRole = async () => {
+            try {
+                const response = await axios.get('/profile');
+                setUserRole(response.data.role); // Ensure you're accessing the correct property
+            } catch (error) {
+                console.error('Error fetching user role:', error);
+            }
+        };
+        fetchUserRole();
+    },[]);
+
+
+    // useEffect(()=>{
+    //     axios.get('/profile').then(response=>{
+    //         setUserRole(response.role)
+    //     })
+    // })
+    
 
     const linkClasses = (page) => {
         
@@ -19,10 +43,12 @@ export default function AccountNavigation(){
         <nav className="w-full flex justify-center mt-8 gap-2 mb-8">
 
             <Link className={linkClasses('profile')} to={'/account'}>My Account</Link>
-            <Link className={linkClasses('adminMovies')} to={'/account/adminMovies'}>My Movies</Link>
-            
-            <Link className={linkClasses('adminTheatres')} to={'/account/adminTheatres'}>My Theatres</Link>
-            <Link className={linkClasses('adminShowtimes')} to={'/account/adminShowtimes'}>My Showtimes</Link>
+            {userRole==='admin' && (<Link className={linkClasses('adminMovies')} to={'/account/adminMovies'}>My Movies</Link>)}
+            {userRole==='admin' && <Link className={linkClasses('adminTheatres')} to={'/account/adminTheatres'}>My Theatres</Link>}
+            {userRole==='admin' && <Link className={linkClasses('adminShowtimes')} to={'/account/adminShowtimes'}>My Showtimes</Link>}
+            {userRole==='superAdmin' && <Link className={linkClasses('superAdmin')} to={'/account/superAdmin'}>superAdmin</Link>}
+            {userRole==='superAdmin' && (<Link className={linkClasses('adminMovies')} to={'/account/adminMovies'}>My Movies</Link>)}
+
 
         </nav>
     );
