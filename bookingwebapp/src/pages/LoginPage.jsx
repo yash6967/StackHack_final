d196b20d27dd08691cff0cbaf3f2b07e64c85530
DarthAtharva <22ucs038@lnmiit.ac.in>
2024-08-25 23:53:44 +0530

    Removed Forget Password and remember me

--- bookingwebapp/src/pages/LoginPage.jsx
+++ bookingwebapp/src/pages/LoginPage.jsx
@@ -177,7 +177,7 @@ export default function LoginPage(){
                                     onChange={ev => setPassword(ev.target.value)} />
                             </div>
                             
-                            <div class="flex items-center justify-between">
+                            {/* <div class="flex items-center justify-between">
                                 <div class="flex items-start">
                                     <div class="flex items-center h-5">
                                         <input id="remember" aria-describedby="remember" type="checkbox" class="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-primary-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-primary-600 dark:ring-offset-gray-800" required=""/>
@@ -187,7 +187,7 @@ export default function LoginPage(){
                                     </div>
                                 </div>
                                 <a href="#" class="text-sm font-medium text-primary-600 hover:underline dark:text-primary-500">Forgot password?</a>
-                            </div>
+                            </div> */}
 
                             <button 
                                 type="submit" 
